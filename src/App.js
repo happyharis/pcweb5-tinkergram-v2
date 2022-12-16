79e8e68ac7e3ab69cfb69b7d3718791bcd2989f9
Haris Samingan <muhamad_haris@hotmail.com>
2022-12-17 07:09:29 +0800

    Add get all posts function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
-
-import "./App.css";
+import { useEffect, useState } from "react";
+import { API, POSTS } from "./constants";
+import axios from "axios";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  async function getAllPosts() {
+    try {
+      const response = await axios.get(API + POSTS);
+      const posts = response.data;
+      console.log(posts);
+      setPosts(posts);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getAllPosts();
+  }, []);
+
   return (
     <Navbar variant="light" bg="light">
       <Container>
