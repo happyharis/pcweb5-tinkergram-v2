8beda2d3aa39deeca504398bfe276d0d8e9cd21e
Haris Samingan <muhamad_haris@hotmail.com>
2022-12-16 19:03:24 +0800

    Add add page

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const API = "https://pcweb5-api.haris-samingan.repl.co";
+export const POSTS = "/posts";
+export const ADD = "/add";
+export const POST = "/post";
+export const DELETE = "/delete";
--- src/views/PostPageAdd.js
+++ src/views/PostPageAdd.js
@@ -1,7 +1,14 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { Button, Container, Form, Nav, Navbar } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { ADD, API } from "../constants";
 
 export default function PostPageAdd() {
+  const [caption, setCaption] = useState("");
+  const [image, setImage] = useState("");
+  const navigate = useNavigate();
+
   return (
     <>
       <Navbar variant="light" bg="light">
@@ -17,17 +24,40 @@ export default function PostPageAdd() {
         <Form>
           <Form.Group className="mb-3" controlId="caption">
             <Form.Label>Caption</Form.Label>
-            <Form.Control type="text" placeholder="Lovely day" />
+            <Form.Control
+              type="text"
+              placeholder="Lovely day"
+              value={caption}
+              onChange={(text) => setCaption(text.target.value)}
+            />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="image">
             <Form.Label>Image URL</Form.Label>
-            <Form.Control type="text" placeholder="https://zca.sg/img/1" />
+            <Form.Control
+              type="text"
+              placeholder="https://zca.sg/img/1"
+              value={image}
+              onChange={(text) => setImage(text.target.value)}
+            />
             <Form.Text className="text-muted">
               Make sure the url has a image type at the end: jpg, jpeg, png.
             </Form.Text>
           </Form.Group>
-          <Button variant="primary">Submit</Button>
+          <Button
+            variant="primary"
+            onClick={async (e) => {
+              const post = { image, caption };
+              try {
+                await axios.post(API + ADD, post);
+                navigate("/");
+              } catch (error) {
+                console.error(error.message);
+              }
+            }}
+          >
+            Submit
+          </Button>
         </Form>
       </Container>
     </>
