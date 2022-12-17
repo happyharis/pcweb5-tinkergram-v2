fd37ff4c8733553308d278ebd7625555312a55b0
Haris Samingan <muhamad_haris@hotmail.com>
2022-12-17 08:24:13 +0800

    Add images row

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
-import { useEffect, useState } from "react";
-import { API, POSTS } from "./constants";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from "react";
+import { Container, Image, Nav, Navbar, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { API, POSTS } from "./constants";
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -21,15 +23,56 @@ function App() {
     getAllPosts();
   }, []);
 
+  const ImagesRow = () => {
+    // posts = [{id: 3, caption: 'Ronaldo', image: 'https://zca.sg/img/1'}];
+    return posts.map((post, index) => <ImageSquare key={index} post={post} />);
+    // return [<ImageSquare key={index} post={post}]
+
+    // return [<ImageSquare
+    //          key={0}
+    //          post={{id: 3, caption: 'Ronaldo', image: 'https://zca.sg/img/1'}}
+    //        />]
+  };
+
   return (
-    <Navbar variant="light" bg="light">
+    <>
+      <Navbar variant="light" bg="light">
+        <Container>
+          <Navbar.Brand href="/">Tinkergram</Navbar.Brand>
+          <Nav>
+            <Nav.Link href="/add">New Post</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
       <Container>
-        <Navbar.Brand href="/">Tinkergram</Navbar.Brand>
-        <Nav>
-          <Nav.Link href="/add">New Post</Nav.Link>
-        </Nav>
+        <Row>
+          <ImagesRow />
+        </Row>
       </Container>
-    </Navbar>
+    </>
+  );
+}
+
+function ImageSquare({ post }) {
+  const { image, id } = post;
+  return (
+    <Link
+      to={`post/${id}`}
+      style={{
+        width: "18rem",
+        marginLeft: "1rem",
+        marginTop: "2rem",
+      }}
+    >
+      <Image
+        src={image}
+        style={{
+          objectFit: "cover",
+          width: "18rem",
+          height: "18rem",
+        }}
+      />
+    </Link>
   );
 }
 
